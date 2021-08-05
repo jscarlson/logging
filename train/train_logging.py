17b23dcb2de8dd8ad61781e00daf8a5a097495ba
jscarlson <jcarlson238@gmail.com>
2021-08-05 00:47:38 -0400

    Add zi2zi logging

--- train/train_logging.py
+++ train/train_logging.py
@@ -6,7 +6,6 @@ import csv
 model = sys.argv[1]
 log_update = sys.argv[2]
 save_path = "../logging/train/log.csv"
-update_counter = 0
 
 if model == "CUT":
 
@@ -29,8 +28,27 @@ if model == "CUT":
                 writer.writerow(log_variables)
             writer.writerow(log_values)
 
-        update_counter += 1
-    
+elif model == "zi2zi":
+
+     if log_update.strip().startswith("Epoch:"):
+
+        log_update = re.sub('\[|\]|[,():/]', ' ', log_update)
+        print(log_update)
+        log_update = log_update.split()
+
+        log_variables = []
+        log_values = []
+        for i in range(0, len(log_update), 2):
+            variable = log_update[i]
+            value = float(log_update[i+1])
+            log_variables.append(variable)
+            log_values.append(value)
+
+        with open(save_path, 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            if os.stat(save_path).st_size == 0:
+                writer.writerow(log_variables)
+            writer.writerow(log_values)    
             
 
 
